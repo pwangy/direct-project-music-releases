ae47c921b9dd0f9e1d8f4a2ac374c5fe9d806c0a
Peggy <64912576+pwangy@users.noreply.github.com>
2020-10-13 12:29:34 +0200

    basic structure defined

--- code/src/Album.js
+++ code/src/Album.js
@@ -1,12 +1,6 @@
 import React from 'react'
 
-
 export const Albums = (props) => {
-
-return <img src={props.url} alt={props.name} width={props.width} height={props.height} key={props.name}></img>
-
-}
-
-
-
-
+  return (
+    <img src={props.url} alt={props.name} className='albumArt'></img>
+  )}
--- code/src/App.js
+++ code/src/App.js
@@ -1,36 +1,26 @@
 import React from 'react'
 import data from './data.json'
+import { Header } from './Header.js'
 import { Albums } from './Album.js'
 import { Artists } from './Artists.js'
+import { ReleaseTitle } from './ReleaseTitle.js'
 
 console.log(data)
 
-
-
-
 export const App = () => {
   return (
-
-    data.albums.items.map((album, subindex) => {
-
-      return <div>
-
-       <Albums url={album.images[0].url} name={album.name} width={album.images[0].width} height={album.images[0].height}/>
-       <Artists key={subindex} albumName={album.name}/>
-
-        { 
-          album.artists.map((sub, subindex) =>
-            <Artists key={subindex} artist={sub.name} />)
-        }
-
-      </div>
-
-    })
-
-  
-
-
-
+    <div className='sectionContainer'>
+      <Header />
+      {data.albums.items.map((album) => {
+        return (
+          <div className='albumContainer'>
+            <Albums key={album.id} url={album.images[0].url} name={album.name} />
+            <ReleaseTitle key={album.name} albumName={album.name} />
+            { album.artists.map((sub, subindex) => <Artists key={subindex} artist={sub.name} />
+            )}
+          </div>
+        )
+      })}
+    </div>
   )
 }
-
