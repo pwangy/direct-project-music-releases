e7c0a516e1e1a913a78a9e71d33db6879e2e66fb
Max Forssén <max.forssen@mentimeter.com>
2020-10-13 15:49:47 +0200

    created container for play button so it can grow without pushing other icons

--- code/src/Album.js
+++ code/src/Album.js
@@ -5,9 +5,11 @@ export const Albums = (props) => {
             <img src={props.url} alt={props.name} className='albumArt'></img>
             <div className="opacityLayer"></div>
             <div className="iconContainer">
-                <img src="/icons/heart.svg" alt="heart icon" className="icons"></img>
+                <img src="/icons/heart.svg" alt="heart icon" className="heartIcon"></img>
+                <div className="playContainer">
                 <img src="/icons/play.svg" alt="play button" className="playButton"></img>
-                <img src="/icons/dots.svg" alt="ellipsis icon" className="icons"></img>
+                </div>
+                <img src="/icons/dots.svg" alt="ellipsis icon" className="ellipsisIcon"></img>
             </div>
         </div>
   )}
