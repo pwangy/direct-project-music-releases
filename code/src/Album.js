2369f2e92461bfc50f6601a9957f7be9607fc07c
Max Forssén <max.forssen@mentimeter.com>
2020-10-13 15:36:39 +0200

    working on hover effects and css in general

--- code/src/Album.js
+++ code/src/Album.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 export const Albums = (props) => {
-  return (
-    <img src={props.url} alt={props.name} className='albumArt'></img>
+  return ( <div className="coverContainer">
+            <img src={props.url} alt={props.name} className='albumArt'></img>
+            <div className="opacityLayer"></div>
+            <div className="iconContainer">
+                <img src="/icons/heart.svg" alt="heart icon" className="icons"></img>
+                <img src="/icons/play.svg" alt="play button" className="playButton"></img>
+                <img src="/icons/dots.svg" alt="ellipsis icon" className="icons"></img>
+            </div>
+        </div>
   )}
