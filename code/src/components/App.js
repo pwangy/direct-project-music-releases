a90936e0a12221b9db5fcd558aa960a73a6ee07a
Max Forssén <max.forssen@mentimeter.com>
2020-10-16 13:41:09 +0200

    fixed kebab-case for classes, fixed key issue, added comments

--- code/src/components/App.js
+++ code/src/components/App.js
@@ -5,17 +5,22 @@ import { Albums } from './Album.js'
 import { Artists } from './Artists.js'
 import { ReleaseTitle } from './ReleaseTitle.js'
 
+//*Main rendering component
 export const App = () => {
   return (
-    <div className='sectionContainer'>
+    <div className='section-container'>
       <Header />
       <section className='rows'>
-      {data.albums.items.map((album) => {
+
+      {/*Mapping through all the albums in the json */}
+      {data.albums.items.map((album, i) => {
         return (
-            <div className='albumContainer'>
+            <div className='album-container' key={i}>
             <Albums key={album.id} url={album.images[0].url} name={album.name} />
             <ReleaseTitle key={album.name} albumName={album.name} albumURL={album.external_urls.spotify} />
-            <div className='artistContainer'>
+            <div className='artist-container'>
+              
+                  {/*Mapping through all the artists in the json to get hold of artist name and their respective artist url */}
                   {album.artists.map((sub, subindex) =>
                     <Artists key={subindex} artist={sub.name} artistURL={sub.external_urls.spotify}/>
             )}
