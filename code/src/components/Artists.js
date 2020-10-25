fcd0d39efc9d37fe9506237d83acff3195c4a9e8
Max Forssén <max.forssen@mentimeter.com>
2020-10-25 16:10:11 +0100

    made suggested changes from Maks feedback

--- code/src/components/Artists.js
+++ code/src/components/Artists.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 //Component to show every artist and its URL
 export const Artists = (props) => {
-   return (
-    <span><a className='artists' href={props.artistURL}>
-        {props.artist}
-    </a></span>
-   )}
+    return (
+        <span>
+            <a className='artists' href={props.artistURL}>
+                {props.artist}
+            </a>
+        </span>
+)}
 
 
    
